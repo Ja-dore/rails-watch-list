8a47bf76ba6f32603b749405582d076ac2fe38df
Dorien Reynders <dorien.reynders@hotmail.com>
2021-08-12 12:44:16 +0200

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+require 'pry'
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+
+info = JSON.parse(URI.open(url).read)
+movie_array = info['results']
+[0, 1, 2, 3, 4].each do |i|
+  movie = movie_array[i]
+
+  Movie.create(title: movie['title'], overview: movie['overview'], poster_url: "https://image.tmdb.org/t/p/original#{movie['poster_path']}", rating: movie['vote_average'])
+end
